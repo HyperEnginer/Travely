6e02a6c3f183b34f38f5b0ced3c517023395ebaa
A <A@sds-MacBook-Pro.lan>
2023-02-25 14:37:34 +0300

    welcome

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import GMap from './screens/maps';
+
+const Travely = () => {
+  return (
+    <View>
+      <GMap/>
+    </View>
+  );
+};
+
+export default Travely;
